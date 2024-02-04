06745a92373a9fb960e1f6bc7fbf04e8dc3bac91
Kaan <kaanasutay10@gmail.com>
2024-02-05 02:38:31 +0300

    env

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,7 +22,7 @@ export const sigin = async (req, res, next) => {
         if (!validUser) return next(errorHandler(404, 'User not found!'))
         const validPassword = bcryptjs.compareSync(password, validUser.password)
         if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'))
-        const token = jwt.sign()
+        const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET)
     } catch (error) {
         next(error)
     }
