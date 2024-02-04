bf9189e143b94d4d63d74e8328125d700f7a07dd
Kaan <kaanasutay10@gmail.com>
2024-02-05 02:45:13 +0300

    sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const signup = async (req, res, next) => {
     }
 };
 
-export const sigin = async (req, res, next) => {
+export const signin = async (req, res, next) => {
     const { email, password } = req.body
     try {
         const validUser = await User.findOne({ email })
@@ -23,6 +23,10 @@ export const sigin = async (req, res, next) => {
         const validPassword = bcryptjs.compareSync(password, validUser.password)
         if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'))
         const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET)
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(validUser)
     } catch (error) {
         next(error)
     }
