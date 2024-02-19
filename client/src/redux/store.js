d49aee920130df23728fe7ced70c40b3ea8e744e
Kaan <kaanasutay10@gmail.com>
2024-02-19 20:18:23 +0300

    user: useReducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useReducer } from './user/userSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {user: useReducer},
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   })
