512d6277f666161e808329c406b6cc510d35e348
Kaan <kaanasutay10@gmail.com>
2024-02-22 19:40:39 +0300

    handleGoogleClick

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const OAuth = () => {
+    const handleGoogleClick = async () => {
+        try {
+            
+        } catch (error) {
+            console.log('coul')
+        }
+    }
+
   return (
-    <div>OAuth</div>
+    <button onClick={handleGoogleClick} type='button' className='bg-red-700  text-white p-3 rounded-lg uppercase hover:opacity-95'> Contiune with google </button>
   )
 }
 
