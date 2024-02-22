eff1085fc8d848cf645bd9991e1117ec0015c199
Kaan <kaanasutay10@gmail.com>
2024-02-22 20:35:30 +0300

    const res

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -8,6 +8,15 @@ export default function OAuth() {
             const auth = getAuth(app)
 
             const result = await signInWithPopup(auth, provider)
+
+            const res = await fetch('/api/auth/google', {
+              method: POST,
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL }),
+            })
+            const data = await res.json()
         } catch (error) {
             console.log('could not sign in with google', error)
         }
