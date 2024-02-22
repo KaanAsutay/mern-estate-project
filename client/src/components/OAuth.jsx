a01be59863c2b70594dd1abfb346ca6a4502e208
Kaan <kaanasutay10@gmail.com>
2024-02-22 20:07:04 +0300

    upgrade OAuth

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from '@firebase/auth'
+import { app } from '../firebase'
 
-const OAuth = () => {
+export default function OAuth() {
     const handleGoogleClick = async () => {
         try {
-            
+            const provider = new GoogleAuthProvider()
+            const auth = getAuth(app)
+
+            const result = await signInWithPopup(auth, provider)
         } catch (error) {
-            console.log('coul')
+            console.log('could not sign in with google', error)
         }
     }
 
   return (
-    <button onClick={handleGoogleClick} type='button' className='bg-red-700  text-white p-3 rounded-lg uppercase hover:opacity-95'> Contiune with google </button>
+    <button 
+    onClick={handleGoogleClick} 
+    type='button' 
+    className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+    > Contiune with google 
+    </button>
   )
 }
-
-export default OAuth
